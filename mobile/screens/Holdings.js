9f4fbbd2486765417cbb207dc1aaa4d12e511fd6
Xtrendence <xtrendence@gmail.com>
2021-03-24 21:37:11 +0000

    Started working on the app's holdings screen.

--- mobile/screens/Holdings.js
+++ mobile/screens/Holdings.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
 import { ThemeContext } from "../utils/theme";
+import { globalColors, globalStyles } from "../styles/global";
+import { empty, separateThousands, abbreviateNumber, epoch } from "../utils/utils";
+
+const screenWidth = Dimensions.get("screen").width;
+const screenHeight = Dimensions.get("screen").height;
 
 export default function Holdings() {
 	const { theme, toggleTheme } = React.useContext(ThemeContext);
 
 	return (
-		<View>
-			<Text>Holdings</Text>
+		<View style={[styles.page, styles[`page${theme}`]]}>
 			<StatusBar style={theme === "Dark" ? "light" : "dark"}/>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	page: {
+		height:screenHeight - 180,
+		backgroundColor:globalColors["Light"].mainSecond,
+		padding:20,
+	},
+	pageDark: {
+		backgroundColor:globalColors["Dark"].mainSecond
+	},
+});

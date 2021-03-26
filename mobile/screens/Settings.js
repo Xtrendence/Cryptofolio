7b1575dbdecd1e00323c8ad1d9fffd40def9b141
Xtrendence <xtrendence@gmail.com>
2021-03-26 21:40:16 +0000

    The app's theme can now be toggled using a switch.

--- mobile/screens/Settings.js
+++ mobile/screens/Settings.js
@@ -2,7 +2,7 @@ import React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationActions } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { Text, TouchableOpacity, View, StyleSheet, ScrollView, Dimensions } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet, ScrollView, Dimensions, Switch } from "react-native";
 import { ThemeContext } from "../utils/theme";
 import { globalColors, globalStyles } from "../styles/global";
 
@@ -16,9 +16,10 @@ export default function Settings({ navigation, route }) {
 		<ScrollView style={[styles.page, styles[`page${theme}`]]} contentContainerStyle={{ padding:20 }} nestedScrollEnabled={true}>
 			<View style={[styles.section, styles[`section${theme}`]]}>
 				<Text style={[styles.header, styles[`header${theme}`]]}>Appearance</Text>
-				<TouchableOpacity onPress={() => switchTheme()} style={styles.button}>
-					<Text style={styles.text}>{ theme === "Dark" ? "Light" : "Dark"} Mode</Text>
-				</TouchableOpacity>
+				<View style={styles.container}>
+					<Text style={[styles.containerText, styles[`containerText${theme}`]]}>Theme</Text>
+					<Switch trackColor={{ false:globalColors["Dark"].mainFifth, true:globalColors["Light"].accentFirst }} thumbColor={theme === "Dark" ? globalColors["Dark"].accentFirst : globalColors["Light"].mainFirst} onValueChange={() => switchTheme()} value={theme !== "Dark"}/>
+				</View>
 			</View>
 			<StatusBar style={theme === "Dark" ? "light" : "dark"}/>
 		</ScrollView>
@@ -51,6 +52,19 @@ const styles = StyleSheet.create({
 	},
 	sectionDark: {
 		backgroundColor:globalColors["Dark"].mainFirst
+	},
+	container: {
+		flexDirection:"row"
+	},
+	containerText: {
+		fontSize:16,
+		color:globalColors["Light"].mainContrast,
+		fontFamily:globalStyles.fontFamily,
+		marginRight:4,
+		paddingTop:1,
+	},
+	containerTextDark: {
+		color:globalColors["Dark"].mainContrast,
 	},
 	header: {
 		fontSize:18,

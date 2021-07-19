2c49e093904b2b5e79715656da0e5e83e8817854
Xtrendence <xtrendence@gmail.com>
2021-07-19 23:52:10 +0100

    Coins can now be removed from watchlist through the dashboard page.

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1770,6 +1770,41 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 					div.innerHTML = '<img draggable="false" src="' + icon + '" title="' + name + '"><span class="coin" title="' + name + '">' + symbol.toUpperCase() + '</span><span class="price">' + currencies[settings.currency] + price + '</span><span class="day">' + priceChangeDay + '%</span>';
 
+					if(settings.dashboardWatchlist === "enabled") {
+						div.classList.add("watchlist-row");
+						
+						let buttonWatchlist = document.createElement("button");
+						buttonWatchlist.classList.add("watchlist-button");
+						buttonWatchlist.innerHTML += '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"/></svg>';
+
+						buttonWatchlist.addEventListener("click", () => {
+							deleteWatchlist(coin.id).then(response => {
+								if("error" in response) {
+									Notify.error({
+										title:"Error",
+										description:response.error
+									});
+								} else {
+									Notify.success({
+										title:"Removed from Watchlist",
+										description:response.message
+									});
+									buttonWatchlist.classList.remove("active");
+								}
+								clearDashboard();
+								listDashboard();
+							}).catch(e => {
+								Notify.error({
+									title:"Error",
+									description:"Couldn't remove coin from watchlist."
+								});
+								console.log(e);
+							});
+						});
+
+						div.appendChild(buttonWatchlist);
+					}
+
 					divDashboardMarketList.appendChild(div);
 				}
 			} catch(e) {
